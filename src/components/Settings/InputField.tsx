ee7fd6749ca616481d56575f5f20eff2ef7c0d22
Ayush-G09 <ayushgokhle@gmail.com>
2024-12-28 15:58:14 +0530

    setting section responsiveness

--- src/components/Settings/InputField.tsx
+++ src/components/Settings/InputField.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 function InputField({title, type}: Props) {
   return (
-    <div style={{ width: '418px', height: '80px', display: 'flex', flexDirection: 'column', boxSizing: 'border-box', justifyContent: 'space-between'}}>
+    <div style={{ width: '100%', maxWidth: '418px', height: '80px', display: 'flex', flexDirection: 'column', boxSizing: 'border-box', justifyContent: 'space-between'}}>
         <Label size='16px' weight={400} color='#232323'>{title}</Label>
         <input type={type} style={{width: '98%', height: '50px', backgroundColor: 'white', border: '1px solid #DFEAF2', outline: 'none', borderRadius: '15px', fontWeight: 400, fontSize: '14px', color: '#718EBF'}}/>
     </div>
--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -5,6 +5,7 @@ import user from '../assets/user.svg';
 import edit from '../assets/edit.png';
 import { breakpoints } from "../utils";
 import InputField from "../components/Settings/InputField";
+import { useMediaQuery } from "react-responsive";
 
 type Option = "Edit Profile" | "Preferences" | "Security";
 
@@ -13,50 +14,43 @@ const Options = ["Edit Profile" , "Preferences" , "Security"] as Option[];
 function Settings() {
 
   const [selected, setSelected] = useState<Option>('Edit Profile');
+  const isTablet = useMediaQuery({ minWidth: breakpoints.tablet });
 
   return (
-    <StyledContainer>
-      <div style={{width: '100%', backgroundColor: 'white', borderRadius: '25px', padding: '30px', boxSizing: 'border-box'}}>
-        <div style={{width: '1050px', display: 'flex', flexDirection: 'column'}}>
-          <div style={{width: '100%', display: 'flex', alignItems: 'end', borderBottom: '1px solid #F4F5F7', gap: '2rem'}}>
+    <StyledSettings>
+      <Container>
+        <div style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
+          <div style={{width: '100%', height: '30px', display: 'flex', alignItems: 'end', borderBottom: '1px solid #F4F5F7', gap: '1rem'}}>
             {Options.map((option) => (<div onClick={() => setSelected(option)} key={option} style={{display: 'flex', flexDirection: 'column', cursor: 'pointer'}}>
-              <Label weight={500} size="16px" color={selected === option ? "#232323" : "#718EBF"} sx={{paddingBottom: '5px', margin: '0 10px'}}>{option}</Label>
+              <Label weight={500} size={isTablet ? "1rem" : "0.8rem"} color={selected === option ? "#232323" : "#718EBF"} sx={{paddingBottom: '5px', margin: '0 10px'}}>{option}</Label>
               <div style={{width: '100%', height: '3px', backgroundColor: selected === option ? '#232323' : 'white', borderTopLeftRadius: '15px', borderTopRightRadius: '15px'}} />
             </div>))}
           </div>
-          {selected === 'Edit Profile' && <>
-          <div style={{width: '100%', maxHeight: '88%', display: 'flex', flexDirection: 'row', marginTop: '2rem'}}>
-            <div style={{height: '100%', width: '15%', display: 'flex', alignItems: 'start', justifyContent: 'center'}}>
-              <div style={{width: '90px', height: '90px', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative'}}>
-                <img src={user} style={{width: '90px', height: '90px'}}/>
-                <img src={edit} style={{width: '30px', height: '30px', position: 'absolute', bottom: 0, right: 0, cursor: 'pointer'}} />
-              </div>
+          {selected === 'Edit Profile' && 
+          <>
+            <Setting>
+              <UserImgCon>
+                <div style={{width: '90px', height: '90px', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative'}}>
+                  <img src={user} style={{width: '90px', height: '90px'}}/>
+                  <img src={edit} style={{width: '30px', height: '30px', position: 'absolute', bottom: 0, right: 0, cursor: 'pointer'}} />
+                </div>
+              </UserImgCon>
+              <Grid>
+                <InputField title="Your Name" type="text" />
+                <InputField title="Username" type="text" />
+                <InputField title="Eamil" type="text" />
+                <InputField title="Password" type="password" />
+                <InputField title="Date of Birth" type="text" />
+                <InputField title="Present Address" type="text" />
+                <InputField title="Permanent Address" type="text" />
+                <InputField title="City" type="text" />
+                <InputField title="Postal Code" type="text" />
+                <InputField title="Country" type="text" />
+              </Grid>
+            </Setting>
+            <div style={{width: '190px', height: '50px', marginLeft: 'auto', marginTop: '40px', backgroundColor: '#232323', borderRadius: '15px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+              <Label weight={500} size="18px" color="white">Save</Label>
             </div>
-            <div
-              style={{
-                maxWidth: '85%',
-                maxHeight: '100%',
-                display: 'grid',
-                gridTemplateColumns: 'repeat(2, 1fr)', // 2 items per row
-                rowGap: '22px',
-                columnGap: '22px',
-              }}
-            >
-              <InputField title="Your Name" type="text" />
-              <InputField title="Username" type="text" />
-              <InputField title="Eamil" type="text" />
-              <InputField title="Password" type="password" />
-              <InputField title="Date of Birth" type="text" />
-              <InputField title="Present Address" type="text" />
-              <InputField title="Permanent Address" type="text" />
-              <InputField title="City" type="text" />
-              <InputField title="Postal Code" type="text" />
-              <InputField title="Country" type="text" />
-            </div>
-          </div>
-          <div style={{width: '190px', height: '50px', marginLeft: 'auto', marginTop: '40px', marginRight: '35px', backgroundColor: '#232323', borderRadius: '15px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <Label weight={500} size="18px" color="white">Save</Label>
-          </div>
           </>}
           {selected === 'Preferences' && 
             <div style={{width: '100%', height: '500px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
@@ -69,12 +63,12 @@ function Settings() {
             </div>
           }
         </div>
-      </div>
-    </StyledContainer>
+      </Container>
+    </StyledSettings>
   );
 }
 
-const StyledContainer = styled.div`  
+const StyledSettings = styled.div`  
   width: 100%;
   height: 100%;
   padding: 10px;
@@ -95,6 +89,57 @@ const StyledContainer = styled.div`
   @media (min-width: ${breakpoints.tablet}) {
       padding: 30px;
     }
+`;
+
+const Container = styled.div`
+width: 100%;
+background-color: white;
+border-radius: 25px;
+padding: 10px;
+box-sizing: border-box;
+
+@media (min-width: ${breakpoints.tablet}) {
+      padding: 30px;
+  }
+`;
+
+const Grid = styled.div`
+width: 100%;
+                  display: grid;
+                  grid-template-columns: repeat(1, 1fr);
+                  row-gap: 22px;
+                  column-gap: 22px;
+                  place-items: center;
+                  @media (min-width: ${breakpoints.tablet}) {
+      grid-template-columns: repeat(2, 1fr);
+  }
+`; 
+
+const Setting = styled.div`
+width: 100%;
+gap: 2rem;
+max-height: 88%;
+display: flex;
+flex-direction: column;
+margin-top: 2rem;
+
+@media (min-width: ${breakpoints.tablet}) {
+      flex-direction: row;
+  }
+`;
+
+const UserImgCon = styled.div`
+height: 100%;
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+
+@media (min-width: ${breakpoints.tablet}) {
+      width: 15%;
+      align-items: start;
+justify-content: start;
+  }
 `;
 
 export default Settings;
